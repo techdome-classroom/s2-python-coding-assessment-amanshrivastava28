db481a950cc727388f6f31215debab9bc6547cf2
Aman <amanshrivastava2804@gmail.com>
2024-10-07 13:56:36 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,17 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stackclass Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
